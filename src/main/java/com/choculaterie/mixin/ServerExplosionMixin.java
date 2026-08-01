f45ef37b8a203761351312c1457cc9a83a73c34d
Choculat <margot@choculaterie.com>
2026-08-01 15:17:29 -0400

    Backwarded to 1.21.2-3

--- src/main/java/com/choculaterie/mixin/ServerExplosionMixin.java
+++ src/main/java/com/choculaterie/mixin/ServerExplosionMixin.java
@@ -5,18 +5,18 @@ import net.minecraft.world.level.ServerExplosion;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ServerExplosion.class)
 public abstract class ServerExplosionMixin {
 
 	@Inject(method = "explode", at = @At("HEAD"))
-	private void ctrlz$begin(CallbackInfoReturnable<Integer> cir) {
+	private void ctrlz$begin(CallbackInfo ci) {
 		BlockHistory.begin();
 	}
 
 	@Inject(method = "explode", at = @At("RETURN"))
-	private void ctrlz$end(CallbackInfoReturnable<Integer> cir) {
+	private void ctrlz$end(CallbackInfo ci) {
 		BlockHistory.end();
 	}
 }
